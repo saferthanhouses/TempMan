47abd2ed808e06105df47f0db2bf1ed29a8d6c0a
Joe Oliver <joey_oliver@hotmail.com>
2019-12-14 22:21:26 -0500

    Add file utils

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,7 +1,8 @@
-import {PathLike, exists, mkdir, writeFile} from "fs";
+import {PathLike, exists, mkdir, writeFile, createReadStream, createWriteStream} from "fs";
 import * as p from  'path'
 import {promisify} from "util";
 import * as path from "path";
+import ncp from "ncp";
 
 const doesExist = promisify(exists);
 const mkDir = promisify(mkdir);
@@ -17,6 +18,29 @@ export async function writeFileDeep(path : PathLike, file : string){
     await writeFilePromisified(path, file, { encoding: "utf-8" });
 }
 
-export function createPathLike(...args : string[]|PathLike[]){
+export function createPathLike(...args : string[]|PathLike[]): string{
     return path.join.apply(null, args);
 }
+
+export function promisifiedCopyFileStream(filePath: string, dest: string){
+    return new Promise((resolve, reject)  => {
+        let rs = createReadStream(filePath);
+        let ws = createWriteStream(dest);
+        rs.pipe(ws);
+        ws.on('finish', ()=>{
+            resolve()
+        })
+        ws.on('error', (err) => reject(err))
+        rs.on('error', (err) => reject(err))
+    })
+}
+
+export function promisifiedNcp(source: string, dest: string){
+    return new Promise((resolve, reject) =>  {
+        ncp(source, dest, (err)=>{
+            if (err) reject(err)
+            resolve();
+        })
+    })
+}
+
